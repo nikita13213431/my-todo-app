5a64a85b78605362c3c2ef92b1b4033c9349dcab
nikita13213431 <abrosimov.nikita04@mail.ru>
2025-06-23 19:24:00 +0300

    задание для стажировки

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { TodoList } from "../components/TodoList";
-import { PostsList } from "../components/PostList"; // Импортируем PostsList
+import { PostsList } from "../components/PostList"; 
 
 export default function HomePage() {
   return (
     <main>
-      <h1>Моё приявление TODO</h1>
-      <h2>Задание 2 PostList</h2>
+      <h1>.</h1>
+      <h2>.</h2>
       
-      {/* Секция для TodoList */}
+      {}
       <section>
         <h3>Список задач</h3>
         <TodoList />
       </section>
 
-      {/* Секция для PostsList */}
+      {}
       <section>
         <h3>Список постов</h3>
         <PostsList />
